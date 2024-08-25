c79f76ccdd61a69071d8c58a9c85c189e325c0e4
mathysard <148949076+mathysard@users.noreply.github.com>
2024-08-25 18:15:40 +0200

    Ajout de l'eventListener "keydown", permettant d'utiliser la calculatrice avec le clavier.

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const error = document.querySelector("#error")
     const closeError = document.querySelector("#closeError");
     let accumulateNumbers = "";
+    const forKeyHandle = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "0": 0,
+        "+": "+",
+        "-": "-",
+        "/": "/",
+        "*": "*",
+        "%": "%"
+    };
 
     buttons.forEach(btn => {
         btn.addEventListener("click", () => {
@@ -43,5 +60,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     closeError.onclick = () => {
         error.style.display = "none";
+    }
+
+    document.onkeydown = e => {
+        for(const [originalKey, pushedKey] of Object.entries(forKeyHandle)) {
+        switch(e.key) {
+            case originalKey:
+                accumulateNumbers += pushedKey;
+                output.textContent = accumulateNumbers;
+                break;
+            }
+        }
+
+        switch(e.key) {
+            case "Backspace":
+                let outputLength = accumulateNumbers.length;
+                if(outputLength <= 1 || outputLength > 1) {
+                    let modifiedLength = accumulateNumbers.slice(0, - 1);
+                    accumulateNumbers = modifiedLength;
+                    output.textContent = modifiedLength;
+                }
+                break;
+            case "=":
+            case "Enter":
+                try {
+                    if(output.textContent !== "") {
+                        const calculation = eval(output.textContent);
+                        output.textContent = calculation;
+                        accumulateNumbers = "";
+                    }
+                } catch(err) {
+                    error.style.display = "block";
+                }
+            break;
+        }
     }
 })
